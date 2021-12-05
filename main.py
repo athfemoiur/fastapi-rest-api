771cd82f58e8b109060da7a5a3ab41c41a764e7e
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-12-05 11:53:12 +0330

    add jwt exception handler

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from starlette.responses import JSONResponse
 
 from routes.auth import auth
 from routes.product import product
@@ -9,3 +11,12 @@ app = FastAPI()
 app.include_router(user)
 app.include_router(product)
 app.include_router(auth)
+
+
+# for handling exception for JWT
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
